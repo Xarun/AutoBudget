17b5d11200f3193a395aa08151fc85a68d715492
Xarun <Xarun@users.noreply.github.com>
2016-03-05 21:39:16 +0100

    Added files via upload

--- AutoBudget.cs
+++ AutoBudget.cs
@@ -11,7 +11,7 @@ namespace AutoBudget
   {
     #region Implementation of IUserMod
 
-    public string Name { get { return "AutoBudget"; } }
+    public string Name { get { return "AutoBudget V2"; } }
     public string Description { get { return "Automatically sets budgets to the lowest possible value to maintain a 'green' status for electricity and water/sewage."; } }
 
     #endregion Implementation of IUserMod
